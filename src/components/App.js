ebce9e4bb81e24ae3d821848153098f6bfae7321
joelchege23 <joelmaina471@gmail.com>
2023-08-31 21:33:45 +0300

    done with lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const appClass = isDarkMode ? "App dark" : "App light";
+
   return (
-    <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+    <div className={appClass}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={handleToggleDarkMode}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+      </header>
+      <ShoppingList items={itemData} />
     </div>
   );
 }
